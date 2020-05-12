8e55d57fbc86564c06753f3da3a8c70e95d63983
vcconline <65193911+vcconline@users.noreply.github.com>
2020-05-12 08:27:38 -0500

    Create ga-tracking.js

---
+++ ga-tracking.js
@@ -0,0 +1,8 @@
+//First part of GA tracking. Last part is in the tracking code settings of the platform
+//put this here so we can use ga function to get how many people will be watching
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', '%YourGATrackingCode%', 'auto');
